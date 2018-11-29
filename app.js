4d3c149c955d476b31e330d16316093143505a9a
Trinal0821 <find@A5-004030.localdomain>
2018-11-28 17:18:12 -0700

    christmas-counter

---
+++ app.js
@@ -0,0 +1,13 @@
+// Countdown to December 25
+
+//html element grabber
+
+//the date for today
+
+//the date we want (December 25)
+
+//calculation to figure out the difference between the days
+
+//display on html
+
+//eventlistener, onchange, the change of the difference
